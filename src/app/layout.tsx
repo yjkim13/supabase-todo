fce4855e1ae1beaa054ffffb721923aaec6fe48e
yjkim13 <sk940913@naver.com>
2025-04-22 20:13:55 +0900

    Fix : build 에러 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,20 @@
 // src/app/layout.tsx
 import "./globals.css";
 
+type LayoutProps = {
+  children: React.ReactNode;
+  modal: React.ReactNode;
+};
+
 export default function RootLayout({
   children,
   modal, // 병렬 라우트 슬롯 이름과 일치해야 함
-}: {
-  children: React.ReactNode;
-  modal?: React.ReactNode;
-}) {
+}: LayoutProps) {
   return (
     <html lang="en">
       <body>
         {children}
-        {modal ?? null}
+        {modal}
       </body>
     </html>
   );
